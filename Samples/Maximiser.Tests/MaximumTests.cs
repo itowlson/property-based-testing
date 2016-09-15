161b52693a519a9d7cec00b96bd0ac9b21a89c50
Ivan Towlson <ivan.towlson@microsoft.com>
2016-09-15 17:17:10 +1200

    Example tests and broken implementations

---
+++ Samples/Maximiser.Tests/MaximumTests.cs
@@ -0,0 +1,51 @@
+﻿using FsCheck.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Maximiser.Tests
+{
+    public class MaximumTests
+    {
+        // Value-based tests
+
+        [Fact]
+        public void MaxValueReturnsTheGreatestValue()
+        {
+            Assert.Equal(3, Maximum.MaxValue(new[] { 3, 0, 0 }));
+        }
+
+        // Property-based tests
+
+        [Property]
+        public void AllValuesAreLessThanOrEqualToTheMax(int[] values)
+        {
+            if (values.Length == 0)
+            {
+                return;  // This is a bit suspicious and we'll come back to it later
+            }
+
+            var max = Maximum.MaxValue(values);
+
+            foreach (var value in values)
+            {
+                Assert.True(value <= max);
+            }
+        }
+
+        [Property]
+        public void TheMaxIsOneOfTheOriginalValues(int[] values)
+        {
+            if (values.Length == 0)
+            {
+                return;
+            }
+
+            var max = Maximum.MaxValue(values);
+            Assert.Contains(max, values);
+        }
+    }
+}
---
+++ Samples/Maximiser/Maximum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximiser
+{
+    public static class Maximum
+    {
+        public static int MaxValue(IEnumerable<int> values)
+        {
+            // Bad implementation 1
+            //return values.First();
+
+            // Bad implementation 2
+            return values.Sum(v => Math.Abs(v));
+        }
+    }
+}

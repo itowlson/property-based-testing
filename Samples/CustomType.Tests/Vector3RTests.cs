25a84c52e84a102974faa3c393181d18eb1304b1
Ivan Towlson <ivan.towlson@microsoft.com>
2016-09-21 11:41:38 +1200

    Show auto generation of types with single constructor

---
+++ Samples/CustomType.Tests/Vector3RTests.cs
@@ -0,0 +1,33 @@
+﻿using FsCheck.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomType.Tests
+{
+    // No generator needed... sorta kinda
+    public class Vector3RTests
+    {
+        [Property]
+        public void VectorLengthIsNonNegative(Vector3R vector)
+        {
+            CompareAssert.Assert(vector.Length).IsGreaterThanOrEqualTo(0);
+        }
+
+        [Property]
+        public void VectorLengthIsAtLeastAsGreatAsAnyComponent(Vector3R vector)
+        {
+            CompareAssert.Assert(vector.Length).IsGreaterThanOrEqualTo(vector.X);
+            CompareAssert.Assert(vector.Length).IsGreaterThanOrEqualTo(vector.Y);
+            CompareAssert.Assert(vector.Length).IsGreaterThanOrEqualTo(vector.Z);
+        }
+
+        [Property]
+        public void TriangleInequalityHolds(Vector3R first, Vector3R second)
+        {
+            CompareAssert.Assert((first + second).Length).IsLessThanOrEqualTo(first.Length + second.Length);
+        }
+    }
+}
---
+++ Samples/CustomType/Vector3R.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomType
+{
+    public class Vector3R
+    {
+        public Vector3R(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        public double Length =>
+            X + Y + Z;                                   // incorrect!
+            // Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);  // taxicab metric
+            // Math.Sqrt(X * X + Y * Y + Z * Z);         // Euclidean metric
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+
+        public static Vector3R operator +(Vector3R first, Vector3R second)
+        {
+            return new Vector3R(first.X + second.X, first.Y + second.Y, first.Z + second.Z);
+        }
+    }
+}
